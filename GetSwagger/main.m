a3969b0482c591a0f2396949af29313fabd36de9
zhuchongyao <zhuchongyao@uml-tech.com>
2020-07-24 09:57:34 +0800

    Initial Commit

---
+++ GetSwagger/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GetSwagger
+//
+//  Created by delims on 2020/7/24.
+//  Copyright © 2020 delims. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
